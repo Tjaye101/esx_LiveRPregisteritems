f1fa6e4acd08ce9046933ba1752b2f129024bc26
Tjaye101 <83477654+Tjaye101@users.noreply.github.com>
2021-08-12 00:04:53 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,2 +1,6 @@
 Config = {}
 Config.Locale = 'en'
+
+-- to change how long tequila lasts go to /server/main.lua:85 (uses miliseconds)
+-- to change how long beer lasts go to /server/main.lua:73 (uses miliseconds)
+-- Both last a minute thirty by default
