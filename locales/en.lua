df13c4ebea95aaba538a8d8f534a2db6f7b5d724
Tjaye101 <83477654+Tjaye101@users.noreply.github.com>
2021-08-12 01:12:06 -0700

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,11 +1,11 @@
 Locales['en'] = {
   ['used_beer'] = 'you have used ~y~1x~s~ ~b~Beer~s~',
-  ['used_burger'] = 'you have used ~y~1x~s~ ~b~Burger~s~',
-  ['used_taco'] = 'you have used ~y~1x~s~ ~b~Taco~s~',
-  ['used_liverpultra'] = 'you have used ~y~1x~s~ ~b~Live RP Ultra Energy~s~',
+  ['used_pizza'] = 'you have used ~y~1x~s~ ~b~Pizza~s~',
+  ['used_hotdog'] = 'you have used ~y~1x~s~ ~b~PHot Dog~s~',
+  ['used_lays'] = 'you have used ~y~1x~s~ ~b~Lays Chips~s~',
   ['used_cocacola'] = 'you have used ~y~1x~s~ ~b~Coca-Cola~s~',
-  ['used_mtndew'] = 'you have used ~y~1x~s~ ~b~Mtn Dew~s~',
-  ['used_spaghetti'] = 'you have used ~y~1x~s~ ~b~Bowl of Spaghetti~s~',
-  ['used_monsterenergy'] = 'you have used ~y~1x~s~ ~b~Bowl of Monster Energy~s~',
+  ['used_iced'] = 'you have used ~y~1x~s~ ~b~Iced tea~s~',
+  ['used_sprite'] = 'you have used ~y~1x~s~ ~b~Sprite~s~',
+  ['used_beer'] = 'you have used ~y~1x~s~ ~b~Beer~s~',
   ['used_tequila'] = 'you have used ~y~1x~s~ ~b~Bowl of Tequila~s~',
 }
